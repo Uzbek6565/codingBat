2a01731de4a3cc197f5b01363a7366b2ec39ed00
Bakhodir <tba-93@yandex.com>
2021-09-22 16:03:35 +0500

    Coding Battle

---
+++ src/main/java/com/example/task_2_1_2/Task212Application.java
@@ -0,0 +1,13 @@
+package com.example.task_2_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task212Application.class, args);
+    }
+
+}
